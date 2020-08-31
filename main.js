a24036e15c3eb1eca538756e35467b281854f329
shishir dhakal <ddshishir88@gmail.com>
2020-08-31 13:01:01 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+//datatypes
+//classification of data
+
+//primitive  data types
+//1. string
+//2. number
+//3. booleannode
+//4.null
+//5, undefined
+
+//data type define gara lai 2 method
+//var and const
+
+/* var name ='shishir';
+console.log('name>>',name);
+name=123;
+console.log('name>>',name);
+name=true;
+console.log('name>>',name);
+name=null;
+console.log('name>>',name);
+
+var shishir;//variable baneraw declare gareu but value assign nagareko
+console.log('name>>>',shishir); 
+ */
+//complex data types
+//1.array
+//2.object
+
+//1.array
+
+/* var name=['shishir',123,,true,null];
+console.log('name>>',name); */
+
+//2.object
+/* var sargam={
+    name:'saragam',
+    phonenumber:9860663278,
+    bool:true,
+    hobbies: ['coding', 'singing'],
+    address: {
+        permanent:'jawlakhel',
+        temporary:'balkumari'
+    }
+}
+console.log("saragam>>>",sargam);
+console.log('saragam phonenumber>>>',sargam.phonenumber);
+console.log('temporary address>>>',sargam.address.temporary); */
+
+
+
